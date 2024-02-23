807cfe8a4f30e452c57a4c3786d718533a25d897
AnaBernard0 <bernardo.catarina32@gmail.com>
2024-02-23 09:32:32 +0000

    Adding models

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly string connectionString;
+        public ProductsController(IConfiguration configuration)
+        {
+            connectionString = configuration["ConnectionStrings:SqlServerDb"] ?? "";
+
+
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; } = "";
+
+        public string Brand { get; set; } = "";
+
+        public string Category { get; set; } = "";
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; } = "";
+    }
+}
